c71b889837df054bd8016e7111b1722f542e81f0
oginer <75859125+oginer@users.noreply.github.com>
2022-12-02 19:55:35 +0100

    clean up

--- Puzzle-02-1/Puzzle-02-1.cpp
+++ Puzzle-02-1/Puzzle-02-1.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <vector>
 
 constexpr unsigned win_score = 6;
 constexpr unsigned draw_score = 3;
@@ -40,55 +41,75 @@ const std::map<const std::pair<const char, const char>, const char> shape_decode
     {{'S', 'Z'}, 'R'}
 };
 
-void do_the_thing(bool real_decode)
+using matches = std::vector<std::pair<char, char>>;
+
+matches parse_input(std::istream &input)
 {
-    std::ifstream input("input.txt");
+    matches data;
+
+    while (input.peek() != EOF)
+    {
+        char p1, p2;
+        input >> p1;
+        if (!input.good()) break;
+        input >> p2;
+        if (!input.good()) break;
+        data.push_back({ p1, p2 });
+    }
+
+    return data;
+}
+
+bool p2_wins(const char p1, const char p2)
+{
+    return (p1 == 'R' && p2 == 'P') ||
+        (p1 == 'P' && p2 == 'S') ||
+        (p1 == 'S' && p2 == 'R');
+}
+
+void do_the_thing(const matches &input, bool real_decode)
+{
     unsigned long score_p1 = 0;
     unsigned long score_p2 = 0;
     unsigned long line = 1;
 
-    while (input.peek() != EOF)
+    for (auto in : input)
     {
-        char p1('-'), p2('-');
-
-        input >> p1 >> p2;
+        char p1, p2;
 
         try
         {
-            p1 = shape_decode.at(p1);
+            p1 = shape_decode.at(in.first);
             if (real_decode)
-                p2 = shape_decode_p2_real.at({ p1, p2 });
+                p2 = shape_decode_p2_real.at({ p1, in.second });
             else
-                p2 = shape_decode.at(p2);
-
-            score_p1 += shape_score.at(p1);
-            score_p2 += shape_score.at(p2);
-
-            if (p1 == p2)
-            {
-                score_p1 += draw_score;
-                score_p2 += draw_score;
-            }
-            else
-            {
-                if ((p1 == 'R' && p2 == 'P') ||
-                    (p1 == 'P' && p2 == 'S') ||
-                    (p1 == 'S' && p2 == 'R'))
-                {
-                    score_p2 += win_score;
-                }
-                else
-                {
-                    score_p1 += win_score;
-                }
-            }
+                p2 = shape_decode.at(in.second);
         }
         catch (...)
         {
-            std::cout << "Error at line " << line << ": p1 = " << p1 << ", p2 = " << p2 << std::endl;
+            std::cout << "Bad input: {" << in.first << ", " << in.second << "}" << std::endl;
+            return;
         }
 
-        ++line;
+        score_p1 += shape_score.at(p1);
+        score_p2 += shape_score.at(p2);
+
+        if (p1 == p2)
+        {
+            score_p1 += draw_score;
+            score_p2 += draw_score;
+        }
+        else
+        {
+            if (p2_wins(p1, p2))
+            {
+                score_p2 += win_score;
+            }
+            else
+            {
+                score_p1 += win_score;
+            }
+        }
     }
 
     std::cout << "Score after " << line - 1 << " matches (" << (real_decode ? "correct" : "incorrect") << " estrategy): " << score_p2 << " (rival score: " << score_p1 << ")" << std::endl;
@@ -96,7 +117,10 @@ void do_the_thing(bool real_decode)
 
 int main()
 {
-    do_the_thing(false);
-    do_the_thing(true);
+    std::ifstream file("input.txt");
+    auto input = parse_input(file);
+
+    do_the_thing(input, false);
+    do_the_thing(input, true);
 }
  
