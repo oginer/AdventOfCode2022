9ac9cb6d511e358b62b55ebb68b6238a6e9d380e
oginer <75859125+oginer@users.noreply.github.com>
2022-12-03 21:58:41 +0100

    Changed loop for set::insert with ranges

--- Puzzle-03-1/Puzzle-03-1.cpp
+++ Puzzle-03-1/Puzzle-03-1.cpp
@@ -4,17 +4,14 @@
 #include <set>
 #include <vector>
 #include <algorithm>
+#include <span>
 
 unsigned short priority(char c)
 {
 	if (c <= 'Z')  // is c uppercase?
-	{
 		return c - 'A' + 27;
-	}
 	else
-	{
 		return c - 'a' + 1;
-	}
 }
 
 int main()
@@ -33,22 +30,15 @@ int main()
 		auto comp_size = rucksacks_size / 2;
 		std::set<char> comp_item_types[2];
 
-		group_backsack[group_index].erase(group_backsack[group_index].begin(), group_backsack[group_index].end());
-
-		for (auto i = comp_size; i > 0; --i)
-		{
-			comp_item_types[0].insert(line[i-1]);
-			comp_item_types[1].insert(line[i+comp_size-1]);
-
-			group_backsack[group_index].insert(line[i - 1]);
-			group_backsack[group_index].insert(line[i + comp_size - 1]);;
-		}
+		comp_item_types[0].insert(line.begin(), line.begin() + comp_size);
+		comp_item_types[1].insert(line.begin()+comp_size, line.end());
 
 		std::vector<char> repeats(1);
-		
 		std::ranges::set_intersection(comp_item_types[0], comp_item_types[1], repeats.begin());
 
 		sum += priority(repeats[0]);
+
+		group_backsack[group_index] = std::set<char>(line.begin(), line.end());
 
 		if (group_index == 2)
 		{
