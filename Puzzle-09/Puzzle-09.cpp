6428ee4f25e283039ae81d1318c2b8100d504b02
oginer <75859125+oginer@users.noreply.github.com>
2022-12-09 19:29:37 +0100

    clean-up and improved input sanitazion

--- Puzzle-09/Puzzle-09.cpp
+++ Puzzle-09/Puzzle-09.cpp
@@ -38,13 +38,13 @@ namespace std
 
 struct movement
 {
-    std::pair<int, int> direction;
-    int distance;
+    std::pair<int8_t, int8_t> direction;
+    unsigned distance;
 
-    static constexpr std::pair<int, int> up {0,1};
-    static constexpr std::pair<int, int> down {0,-1};
-    static constexpr std::pair<int, int> left {-1,0};
-    static constexpr std::pair<int, int> right {1,0};
+    static constexpr decltype(direction) up {0,1};
+    static constexpr decltype(direction) down {0,-1};
+    static constexpr decltype(direction) left {-1,0};
+    static constexpr decltype(direction) right {1,0};
 };
 
 std::vector<movement> parse_input(std::istream& input)
@@ -58,26 +58,26 @@ std::vector<movement> parse_input(std::istream& input)
 
         auto command = parse_command(line);
 
-        switch (command[0][0])
+        if (command.size() == 2 && std::isdigit(command[1][0]))
         {
-        case 'U':
-            mov.direction = movement::up;
-            break;
-        case 'D':
-            mov.direction = movement::down;
-            break;
-        case 'L':
-            mov.direction = movement::left;
-            break;
-        case 'R':
-            mov.direction = movement::right;
-            break;
-        default:
-            continue;
+            switch (command[0][0])
+            {
+            case 'U':
+                mov.direction = movement::up;
+                    break;
+            case 'D':
+                mov.direction = movement::down;
+                    break;
+            case 'L':
+                mov.direction = movement::left;
+                break;
+            case 'R':
+                mov.direction = movement::right;
+                break;
+            }
+            mov.distance = std::stoi(command[1]);
+            result.push_back(mov);
         }
-
-        mov.distance = std::stoi(command[1]);
-        result.push_back(mov);
     }
 
     return result;
@@ -85,43 +85,40 @@ std::vector<movement> parse_input(std::istream& input)
 
 bool tail_touches_head(const position &head, const position &tail )
 {
-    if (std::abs(tail.x - head.x) <= 1 && std::abs(tail.y - head.y) <= 1)
-        return true;
-    else
-        return false;
+    return std::abs(tail.x - head.x) <= 1 && std::abs(tail.y - head.y) <= 1;
 }
 
 void fix_tail(const position& head, position& tail)
 {
-    const int dx = tail.x - head.x;
-    const int dy = tail.y - head.y;
+    if (!tail_touches_head(head, tail))
+    {
+        const int dx = tail.x - head.x;
+        const int dy = tail.y - head.y;
 
-    if (dx > 0) tail.x--;
-    else if (dx < 0) tail.x++;
-    if (dy > 0) tail.y--;
-    else if (dy < 0) tail.y++;
+        if (dx > 0) --tail.x;
+        else if (dx < 0) ++tail.x;
+        if (dy > 0) --tail.y;
+        else if (dy < 0) ++tail.y;
+    }
 }
 
-void simulate_rope(const std::vector<movement>& input, const int n_knots)
+void simulate_rope(const std::vector<movement>& input, const unsigned n_knots)
 {
-    std::unordered_set<position> tail_positions = { {0,0} };
-    std::vector<position> rope(n_knots, { 0,0 });
+    std::unordered_set<position> tail_positions = {{0, 0}};
+    std::vector<position> rope(n_knots, {0, 0});
 
     for (auto &command : input)
     {
-        for (auto i : std::views::iota(0, command.distance))
-        {
+        for (unsigned i = 0; i<command.distance; ++i)
+            {
             rope[0] += command.direction;
 
-            for (int i: std::views::iota(1, n_knots))
+            for (size_t j = 1; j<n_knots; ++j)
             {
-                if (!tail_touches_head(rope[i-1], rope[i]))
-                {
-                    fix_tail(rope[i-1], rope[i]);
-                }
+                fix_tail(rope[j-1], rope[j]);
             }
             tail_positions.insert(rope[n_knots-1]);
-       }
+        }
     }
 
     std::cout << "Number of tail positions (rope with " << n_knots << " knots): " << tail_positions.size() << std::endl;
