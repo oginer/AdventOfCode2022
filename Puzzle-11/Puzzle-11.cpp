b5172412803b6dad2bda411eee112c6b1807e50e
oginer <75859125+oginer@users.noreply.github.com>
2022-12-11 21:53:34 +0100

    clean-up and refactoring

--- Puzzle-11/Puzzle-11.cpp
+++ Puzzle-11/Puzzle-11.cpp
@@ -5,10 +5,10 @@
 #include <vector>
 #include <numeric>
 #include <ranges>
-#include <set>
 
 #include "../utils/timer.h"
 #include "../utils/parse_helper.h"
+
 
 struct Operation
 {
@@ -26,6 +26,31 @@ struct Monkey
     unsigned inspections = 0;
 };
 
+Operation parse_operation(const std::vector<std::string> &command)
+{
+    Operation result;
+
+    switch (command[4][0])
+    {
+    case '+':
+        result.operand = Operation::sum;
+        result.param = std::stol(command[5]);
+        break;
+    case '*':
+        if (command[5] == "old")
+        {
+            result.operand = Operation::pow2;
+        }
+        else
+        {
+            result.operand = Operation::mult;
+            result.param = std::stol(command[5]);
+        }
+    }
+
+    return result;
+}
+
 Monkey parse_monkey(std::istream& input)
 {
     Monkey result;
@@ -38,28 +63,12 @@ Monkey parse_monkey(std::istream& input)
         {
             for (size_t i = 2; i < command.size(); ++i)
             {
-                result.items.push(std::stoll(command[i]));
+                result.items.push(std::stol(command[i]));
             }
         }
         else if (command.size() == 6 && command[0] == "Operation:")
         {
-            switch (command[4][0])
-            {
-            case '+':
-                result.op.operand = Operation::sum;
-                result.op.param = std::stol(command[5]);
-                break;
-            case '*':
-                if (command[5] == "old")
-                {
-                    result.op.operand = Operation::pow2;
-                }
-                else
-                {
-                    result.op.operand = Operation::mult;
-                    result.op.param = std::stol(command[5]);
-                }
-            }
+            result.op = parse_operation(command);
         }
         else if (command.size() == 4 && command[0] == "Test:")
         {
@@ -100,7 +109,6 @@ std::vector<Monkey> parse_input(std::istream&& input)
 
 void monkey_round(std::vector<Monkey>& input, bool divide_by_3, unsigned lcm)
 {
-
     for (Monkey& monkey : input)
     {
         while (!monkey.items.empty())
@@ -123,36 +131,25 @@ void monkey_round(std::vector<Monkey>& input, bool divide_by_3, unsigned lcm)
             if (divide_by_3) item /= 3;
             else item %= lcm;
 
-            if (item % monkey.divisible_by == 0)
-            {
-                input[monkey.throw_if_true].items.push(item);
-            }
-            else
-            {
-                input[monkey.throw_if_false].items.push(item);
-            }
+            input[(item % monkey.divisible_by == 0) ? monkey.throw_if_true : monkey.throw_if_false].items.push(item);
         }
     }
 }
 
 void monkey_play(std::vector<Monkey> input, unsigned rounds, bool divide_by_3)
 {
-    unsigned lcm = std::reduce(input.cbegin(), input.cend(), 1, [](unsigned acc, const Monkey& m) {return std::lcm(acc, m.divisible_by); });
+    unsigned lcm = std::reduce(input.cbegin(), input.cend(), 1, [](unsigned a, const Monkey& m) {return std::lcm(a, m.divisible_by); });
 
-    for (unsigned i = 0; i < rounds; ++i)
+    for (unsigned i = rounds; i>0; --i)
     {
         monkey_round(input, divide_by_3, lcm);
     }
 
-    std::set<unsigned> inspections;
-    for (const Monkey& m : input)
-    {
-        inspections.insert(m.inspections);
-    }
+    std::partial_sort(input.begin(), input.begin() + 2, input.end(), [](const Monkey& m1, const Monkey& m2) {return m1.inspections > m2.inspections; });
 
-    std::partial_sort(input.begin(), input.begin() + 2, input.end(), [](const Monkey& m1, const Monkey& m2) -> bool {return m1.inspections > m2.inspections; });
+    unsigned long long monkey_business = static_cast<unsigned long long>(input[0].inspections) * input[1].inspections;
 
-    std::cout << "Monkey business: " << static_cast<unsigned long long>(input[0].inspections) * input[1].inspections << std::endl;
+    std::cout << "Monkey business: " << monkey_business << std::endl;
 }
 
 int main()
