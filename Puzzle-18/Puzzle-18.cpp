f3176a43580a5094387a32ebe305411597eec986
oginer <75859125+oginer@users.noreply.github.com>
2022-12-18 07:59:00 +0100

    Part 2 finished

--- Puzzle-18/Puzzle-18.cpp
+++ Puzzle-18/Puzzle-18.cpp
@@ -1,11 +1,14 @@
 #include <vector>
 #include <array>
+#include <stack>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <unordered_set>
+#include <cassert>
 
 #include "../utils/parse_helper.h"
+#include "../utils/timer.h"
 
 
 struct cube
@@ -16,16 +19,8 @@ struct cube
 	{
 		return { x + c.x, y + c.y, z + c.z };
 	}
-	cube operator- (const cube& c) const
-	{
-		return { x - c.x, y - c.y, z - c.z };
-	}
 
 	bool operator== (const cube&) const = default;
-
-//	cube(int _x, int _y, int _z)
-//		:x(_x), y(_y), z(_z)
-//	{}
 };
 
 struct hash_cube
@@ -54,6 +49,72 @@ std::vector<cube> parse_input(std::istream& input)
 	return result;
 }
 
+constexpr size_t side = 25;
+
+unsigned fill(int area[side][side][side], const cube &start, short val)
+{
+	std::stack<cube> open;
+	open.push(start);
+
+	unsigned long count = 0;
+
+	while (!open.empty())
+	{
+		auto current = open.top();
+		open.pop();
+
+		if (area[current.x][current.y][current.z] == 0)
+		{
+			area[current.x][current.y][current.z] = val;
+
+			std::array<cube, 6> adjacents
+			{
+				current + cube{1,0,0},
+				current + cube{-1,0,0},
+				current + cube{0,1,0},
+				current + cube{0,-1,0},
+				current + cube{0,0,1},
+				current + cube{0,0,-1}
+			};
+
+			for (auto& adj : adjacents)
+			{
+				if (adj.x >= 0 && adj.x < side &&
+					adj.y >= 0 && adj.y < side &&
+					adj.z >= 0 && adj.z < side)
+				{
+					int adj_val = area[adj.x][adj.y][adj.z];
+					if (adj_val == 1) ++count;
+					if (adj_val == 0) open.push(adj);
+				}
+			}
+		}
+	};
+
+	return count;
+}
+
+void part2(const std::vector<cube>& input)
+{
+
+	int area[side][side][side];
+	std::memset(area, 0, sizeof(area));
+
+	unsigned long count = 0;
+
+	for (auto& c : input)
+	{
+		assert(c.x+2 < side);
+		assert(c.y+2 < side);
+		assert(c.z+2 < side);
+		area[c.x+1][c.y+1][c.z+1] = 1;
+	}
+
+	count = fill(area, { 0,0,0 }, 2);
+
+	std::cout << "Surface area: " << count << std::endl;
+}
+
 void part1(const std::vector<cube>& input)
 {
 	std::unordered_set<cube, hash_cube> cubes;
@@ -67,11 +128,11 @@ void part1(const std::vector<cube>& input)
 			std::array<cube, 6> adjacents
 			{
 				c + cube{1,0,0},
-				c - cube{1,0,0},
+				c + cube{-1,0,0},
 				c + cube{0,1,0},
-				c - cube{0,1,0},
+				c + cube{0,-1,0},
 				c + cube{0,0,1},
-				c - cube{0,0,1}
+				c + cube{0,0,-1}
 			};
 
 			count += 6;
@@ -94,7 +155,17 @@ std::vector<cube> parse_input(std::istream&& input)
 
 int main()
 {
-	auto input = parse_input(std::ifstream("input.txt"));
+	Timer t("Total");
 
+	Timer ti("Input");
+	auto input = parse_input(std::ifstream("input.txt"));
+	ti.finish(false);
+
+	Timer t1;
 	part1(input);
+	t1.finish();
+
+	Timer t2;
+	part2(input);
+	t2.finish();
 }
